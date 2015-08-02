9dbd11b3ab204fe64326d2029c79cf2936e3b2dc
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-02 17:09:34 +0430

    Change OAuth Namespace

---
+++ Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rad\OAuthentication;
+
+use Rad\Core\Exception\BaseException;
+
+/**
+ * OAuth Exception
+ *
+ * @package Rad\OAuthentication
+ */
+class Exception extends BaseException
+{
+
+}
---
+++ OAuthProviderFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Rad\OAuthentication;
+
+use Rad\Utility\Inflection;
+
+/**
+ * OAuth Provider Factory
+ *
+ * @package Rad\OAuthentication
+ */
+class OAuthProviderFactory
+{
+    /**
+     * Create OAuth provider
+     *
+     * @param string $oAuthProvider
+     *
+     * @return AbstractOAuthProvider
+     * @throws Exception
+     */
+    public static function create($oAuthProvider)
+    {
+        if (strpos($oAuthProvider, '\\') !== false) {
+            if (class_exists($oAuthProvider)) {
+                $providerInstance = new $oAuthProvider();
+                if (!($providerInstance instanceof AbstractOAuthProvider)) {
+                    throw new Exception(
+                        sprintf(
+                            'OAuth provider "%s" must be extend "AbstractOAuthProvider".',
+                            $oAuthProvider
+                        )
+                    );
+                }
+
+                return $providerInstance;
+            } else {
+                throw new Exception(sprintf('OAuth provider "%s" does not exist.', $oAuthProvider));
+            }
+        }
+
+        $oAuthClass = 'Rad\\Security\\Authentication\\OAuth\\Provider\\' .
+            Inflection::camelize($oAuthProvider) . 'Provider';
+
+        if (class_exists($oAuthClass)) {
+            return new $oAuthClass();
+        } else {
+            throw new Exception(sprintf('OAuth provider "%s" does not exist.', $oAuthClass));
+        }
+    }
+}
